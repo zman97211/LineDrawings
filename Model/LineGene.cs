37067767494398318491bb0c9b96dd7462feed7a
Steve Ziuchkovski <zman97211@gmail.com>
2017-02-25 16:13:06 -0500

    Simple gene and phenotype expression of a string of genes.

---
+++ Model/LineGene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+    public class LineGene
+    {
+        public byte x1 { get; set; }
+        public byte y1 { get; set; }
+        public byte x2 { get; set; }
+        public byte y2 { get; set; }
+
+        public static byte[,] GetPhenotype(IEnumerable<LineGene> lines)
+        {
+            var phenotype = new byte[256, 256];
+            foreach (var line in lines)
+                Algorithms.Line(line.x1, line.y1, line.x2, line.y2, (x, y) => { phenotype[x, y] = 0; return true; });
+            return phenotype;
+        }
+    }
+}
