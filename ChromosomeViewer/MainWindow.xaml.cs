cb3fb6f44344c02f692331eec4e5bf8dfe742b35
Steve Ziuchkovski <zman97211@gmail.com>
2017-02-25 16:37:01 -0500

    Simple WPF app shows random chromosome image.

---
+++ ChromosomeViewer/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Model;
+
+namespace ChromosomeViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static Bitmap GenBitmap(byte[,] c)
+        {
+            var width = c.GetLength(0);
+            var height = c.GetLength(1);
+            var bitmap = new Bitmap(width, height);
+            for (var y = 0; y < height; ++y)
+                for (var x = 0; x < width; ++x)
+                {
+                    var val = c[x, y];
+                    bitmap.SetPixel(x, y, Color.FromArgb(val, val, val));
+                }
+            return bitmap;
+        }
+
+        public BitmapImage Phenotype { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var c = LineGene.GetRandomChromosome(100);
+            var p = LineGene.GetPhenotype(c);
+            var b = GenBitmap(p);
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                b.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                Phenotype = new BitmapImage();
+                Phenotype.BeginInit();
+                Phenotype.StreamSource = memory;
+                Phenotype.CacheOption = BitmapCacheOption.OnLoad;
+                Phenotype.EndInit();
+            }
+
+            DataContext = this;
+        }
+    }
+}
--- Model/LineGene.cs
+++ Model/LineGene.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Model
 {
@@ -9,10 +10,31 @@ namespace Model
         public byte x2 { get; set; }
         public byte y2 { get; set; }
 
-        public static byte[,] GetPhenotype(IEnumerable<LineGene> lines)
+        static Random _rand = new Random();
+
+        public LineGene()
+        {
+            x1 = (byte)_rand.Next(255);
+            y1 = (byte)_rand.Next(255);
+            x2 = (byte)_rand.Next(255);
+            y2 = (byte)_rand.Next(255);
+        }
+
+        public static IEnumerable<LineGene> GetRandomChromosome(int length)
+        {
+            var chromosome = new List<LineGene>(length);
+            for (var i = 0; i < length; ++i)
+                chromosome.Add(new LineGene());
+            return chromosome;
+        }
+
+        public static byte[,] GetPhenotype(IEnumerable<LineGene> chromosome)
         {
             var phenotype = new byte[256, 256];
-            foreach (var line in lines)
+            for (var y = 0; y < 256; ++y)
+                for (var x = 0; x < 256; ++x)
+                    phenotype[x, y] = 255;
+            foreach (var line in chromosome)
                 Algorithms.Line(line.x1, line.y1, line.x2, line.y2, (x, y) => { phenotype[x, y] = 0; return true; });
             return phenotype;
         }
