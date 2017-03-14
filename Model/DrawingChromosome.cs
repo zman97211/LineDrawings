a3742168ca3ae3948d1d32a0aee78ede7902226d
Steve Ziuchkovski <zman97211@gmail.com>
2017-03-13 21:11:39 -0400

    Move to different framework.

---
+++ Model/DrawingChromosome.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using GeneticSharp.Domain.Chromosomes;
+
+namespace Model
+{
+    class DrawingChromosome : HasIdBase, IChromosome
+    {
+        private readonly List<Gene> _genes;
+
+        public DrawingChromosome(int length)
+        {
+            _genes = new List<Gene>(length);
+            for (var i = 0; i < length; ++i)
+                _genes.Add(new Gene(new LineGene()));
+        }
+
+        public int CompareTo(IChromosome other)
+        {
+            return Id.CompareTo(((DrawingChromosome)other).Id);
+        }
+
+        public Gene GenerateGene(int geneIndex)
+        {
+            return new Gene(new LineGene());
+        }
+
+        public void ReplaceGene(int index, Gene gene)
+        {
+            _genes[index] = gene;
+        }
+
+        public void ReplaceGenes(int startIndex, Gene[] genes)
+        {
+            _genes.RemoveRange(startIndex, genes.Length);
+            _genes.InsertRange(startIndex, genes);
+        }
+
+        public void Resize(int newLength)
+        {
+            if (_genes.Count > newLength)
+                _genes.RemoveRange(newLength, _genes.Count - newLength);
+            else
+            {
+                var nToAdd = newLength - _genes.Count;
+                for (int i = 0; i < nToAdd; ++i)
+                    _genes.Add(new Gene(new LineGene()));
+            }
+        }
+
+        public Gene GetGene(int index)
+        {
+            return _genes[index];
+        }
+
+        public Gene[] GetGenes()
+        {
+            return _genes.ToArray();
+        }
+
+        public IChromosome CreateNew()
+        {
+            return new DrawingChromosome(100);
+        }
+
+        public IChromosome Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? Fitness { get; set; }
+
+        public int Length => _genes.Count;
+
+        public byte[,] GetPhenotype()
+        {
+            var phenotype = new byte[256, 256];
+            for (var y = 0; y < 256; ++y)
+                for (var x = 0; x < 256; ++x)
+                    phenotype[x, y] = 255;
+            foreach (var lineGene in _genes)
+            {
+                var line = (LineGene) lineGene.Value;
+                Algorithms.Line(line.x1, line.y1, line.x2, line.y2, (x, y) =>
+                {
+                    phenotype[x, y] = 0;
+                    return true;
+                });
+            }
+            return phenotype;
+        }
+    }
+
+    internal class HasIdBase
+    {
+        private static int _nextId;
+        private static readonly Object _nextIdLock = new Object();
+        public HasIdBase()
+        {
+            lock (_nextIdLock)
+            {
+                Id = _nextId++;
+            }
+        }
+
+        public int Id { get; private set; }
+    }
+}
--- Model/LineGene.cs
+++ Model/LineGene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Model
 {
-    public class LineGene
+    public class LineGene 
     {
         public byte x1 { get; set; }
         public byte y1 { get; set; }
@@ -18,25 +18,6 @@ namespace Model
             y1 = (byte)_rand.Next(255);
             x2 = (byte)_rand.Next(255);
             y2 = (byte)_rand.Next(255);
-        }
-
-        public static IEnumerable<LineGene> GetRandomChromosome(int length)
-        {
-            var chromosome = new List<LineGene>(length);
-            for (var i = 0; i < length; ++i)
-                chromosome.Add(new LineGene());
-            return chromosome;
-        }
-
-        public static byte[,] GetPhenotype(IEnumerable<LineGene> chromosome)
-        {
-            var phenotype = new byte[256, 256];
-            for (var y = 0; y < 256; ++y)
-                for (var x = 0; x < 256; ++x)
-                    phenotype[x, y] = 255;
-            foreach (var line in chromosome)
-                Algorithms.Line(line.x1, line.y1, line.x2, line.y2, (x, y) => { phenotype[x, y] = 0; return true; });
-            return phenotype;
         }
     }
 }
