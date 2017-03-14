289b8b25438a206c1de7316bb51f33211fdcee96
Steve Ziuchkovski <zman97211@gmail.com>
2017-03-14 19:33:52 -0400

    Add WPF skeleton app, remove old test viewer.

--- LineDrawingsGui/App.xaml.cs
+++ LineDrawingsGui/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ChromosomeViewer
+namespace LineDrawingsGui
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- LineDrawingsGui/Properties/Resources.Designer.cs
+++ LineDrawingsGui/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ChromosomeViewer.Properties
+namespace LineDrawingsGui.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace ChromosomeViewer.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ChromosomeViewer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LineDrawingsGui.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
