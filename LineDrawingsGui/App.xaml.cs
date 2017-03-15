70967ba48f148cafbe6c76b02941a1a441d7c9cc
Steve Ziuchkovski <zman97211@gmail.com>
2017-03-14 20:37:22 -0400

    GUI works (no preview). Removed old console project.

--- LineDrawingsGui/App.xaml.cs
+++ LineDrawingsGui/App.xaml.cs
@@ -13,5 +13,11 @@ namespace LineDrawingsGui
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var vm = new MainWindowViewModel();
+            var win = new MainWindow {DataContext = vm};
+            win.Show();
+        }
     }
 }
--- LineDrawingsGui/MainWindowViewModel.cs
+++ LineDrawingsGui/MainWindowViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using GeneticSharp.Domain;
 using GeneticSharp.Domain.Crossovers;
 using GeneticSharp.Domain.Populations;
@@ -13,11 +18,11 @@ namespace LineDrawingsGui
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
-        private string _inputFilename;
-        private string _outputFilename;
-        private int _numberOfGenerations;
-        private int _numberOfLines;
-        private double _mutationProbability;
+        private string _inputFilename = @"d:\pic.jpg";
+        private string _outputFilename = @"d:\out.png";
+        private int _numberOfGenerations = 2000;
+        private int _numberOfLines = 100;
+        private float _mutationProbability = 0.1f;
         private int _currentGeneration;
         private double _progress;
 
@@ -45,7 +50,7 @@ namespace LineDrawingsGui
             set { _numberOfLines = value; OnPropertyChanged(); }
         }
 
-        public double MutationProbability
+        public float MutationProbability
         {
             get { return _mutationProbability; }
             set { _mutationProbability = value; OnPropertyChanged(); }
@@ -63,6 +68,12 @@ namespace LineDrawingsGui
             set { _progress = value; OnPropertyChanged(); }
         }
 
+        public BitmapImage BestChromosome
+        {
+            get { return _bestChromosome; }
+            set { _bestChromosome = value; OnPropertyChanged(); }
+        }
+
         public RelayCommand StartCommand { get; set; }
         public RelayCommand BrowseInputFileCommand { get; set; }
         public RelayCommand BrowseOutputFileCommand { get; set; }
@@ -76,6 +87,7 @@ namespace LineDrawingsGui
 
         private GeneticAlgorithm _ga;
         private Thread _gaThread;
+        private BitmapImage _bestChromosome;
 
         private void StartGa()
         {
@@ -88,17 +100,56 @@ namespace LineDrawingsGui
 
             _ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
             _ga.Termination = new GenerationNumberTermination(NumberOfGenerations);
+            _ga.MutationProbability = MutationProbability;
 
             _ga.GenerationRan += OnGenerationComplete;
+            _ga.TerminationReached += OnTerminationReached;
 
             _gaThread = new Thread(() => _ga.Start());
             _gaThread.Start();
+        }
+
+        private void OnTerminationReached(object sender, EventArgs eventArgs)
+        {
+            var bitmap = GenBitmap((DrawingChromosome)_ga.BestChromosome);
+            bitmap.Save(OutputFilename);
         }
 
         private void OnGenerationComplete(object sender, EventArgs e)
         {
             CurrentGeneration = _ga.GenerationsNumber;
             Progress = (double)CurrentGeneration / NumberOfGenerations;
+        }
+
+        private void SetBestChromosome(DrawingChromosome c)
+        {
+            var bitmap = GenBitmap(c);
+
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                BestChromosome = bitmapImage;
+            }
+        }
+
+        private static Bitmap GenBitmap(DrawingChromosome c)
+        {
+            // REVISIT: This is a crappy way of doing this.
+            var bitmap = new Bitmap(256, 256);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.FillRectangle(Brushes.White, 0, 0, 255, 255);
+            var ph = c.GetPhenotype();
+            for (var y = 0; y < 256; ++y)
+            for (var x = 0; x < 256; ++x)
+                if (ph[x, y] < 128)
+                    graphics.DrawLine(Pens.Black, x, y, x + 1, y + 1);
+            return bitmap;
         }
 
         private void BrowseInputFile()
