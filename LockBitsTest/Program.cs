f52fe5a1ea8e89a60df71c6f9c0e8ea65a5fddcb
Steve Ziuchkovski <zman97211@gmail.com>
2017-03-16 21:26:03 -0400

    Disk -> FastImage -> FastImage -> Disk works.

--- LockBitsTest/Program.cs
+++ LockBitsTest/Program.cs
@@ -18,26 +18,53 @@ namespace LockBitsTest
     {
         private const int BytesPerPixel = 4;
         private readonly byte[] _buffer;
+        private int _width;
+        private int _height;
+
+        public FastImage(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            _buffer = new byte[_width * _height * BytesPerPixel + 1024 * 1024 * 256];
+        }
 
         public FastImage(Bitmap from)
         {
-            _buffer = new byte[from.Width * from.Height * BytesPerPixel + 1024 * 1024 * 256];
+            _width = from.Width;
+            _height = from.Height;
+            _buffer = new byte[_width * _height * BytesPerPixel + 1024 * 1024 * 256];
             GCHandle handle = GCHandle.Alloc(_buffer);
             try
             {
                 var address = Marshal.UnsafeAddrOfPinnedArrayElement(_buffer, 0);
                 var bitsPerPixel = 32;
-                var stride = bitsPerPixel * from.Width;
+                var stride = bitsPerPixel * _width;
                 var pixelFormat = PixelFormat.Format32bppArgb;
-                using (var fastBitmap = new Bitmap(from.Width, from.Height, stride, pixelFormat, address))
+                using (var fastBitmap = new Bitmap(_width, _height, stride, pixelFormat, address))
                 {
                     using (var fastBitmapGraphics = Graphics.FromImage(fastBitmap))
                         fastBitmapGraphics.DrawImageUnscaled(from, 0, 0);
                 }
             }
-            catch
+            finally
             {
-                throw;
+                handle.Free();
+            }
+        }
+
+        public void Save(string filename)
+        {
+            GCHandle handle = GCHandle.Alloc(_buffer);
+            try
+            {
+                var address = Marshal.UnsafeAddrOfPinnedArrayElement(_buffer, 0);
+                var bitsPerPixel = 32;
+                var stride = bitsPerPixel * _width;
+                var pixelFormat = PixelFormat.Format32bppArgb;
+                using (var fastBitmap = new Bitmap(_width, _height, stride, pixelFormat, address))
+                {
+                    fastBitmap.Save(filename);
+                }
             }
             finally
             {
@@ -49,6 +76,15 @@ namespace LockBitsTest
         {
             var i = y * BytesPerPixel + x;
             return Color.FromArgb(_buffer[i], _buffer[i + 1], _buffer[i + 2], _buffer[i + 3]);
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            var i = y * BytesPerPixel + x;
+            _buffer[i] = color.A;
+            _buffer[i + 1] = color.R;
+            _buffer[i + 2] = color.G;
+            _buffer[i + 3] = color.B;
         }
     }
 
@@ -64,10 +100,10 @@ namespace LockBitsTest
 
             var original = new Bitmap(@"d:\pic.jpg");
             var fast = new FastImage(original);
-            var fresh = new Bitmap(original.Width, original.Height, PixelFormat.Format32bppArgb);
+            var fresh = new FastImage(original.Width, original.Height);
             for (var y = 0; y < original.Height; ++y)
                 for (var x = 0; x < original.Width; ++x)
-                    fresh.SetPixel(x, y, original.GetPixel(x, y));
+                    fresh.SetPixel(x, y, fast.GetPixel(x, y));
             fresh.Save(@"d:\pic_copy.jpg");
 
 
